e266985144fbda88858f25ef0813295f0298b972
akihatake <akihatake@gmail.com>
2017-09-30 13:50:36 +0900

    それらしい値に変更

--- pocket-money/src/MyGraph.js
+++ pocket-money/src/MyGraph.js
@@ -34,25 +34,17 @@ export default class MyGraph extends React.Component {
                 name: 'Brands',
                 colorByPoint: true,
                 data: [{
-                    name: 'Microsoft Internet Explorer',
-                    y: 56.33
+                    name: 'お菓子',
+                    y: 10
                 }, {
-                    name: 'Chrome',
-                    y: 24.03,
-                    sliced: true,
-                    selected: true
+                    name: '文具',
+                    y: 14
                 }, {
-                    name: 'Firefox',
-                    y: 10.38
+                    name: '本',
+                    y: 60
                 }, {
-                    name: 'Safari',
-                    y: 4.77
-                }, {
-                    name: 'Opera',
-                    y: 0.91
-                }, {
-                    name: 'Proprietary or Undetectable',
-                    y: 0.2
+                    name: '雑貨',
+                    y: 16
                 }]
             }]
         };
